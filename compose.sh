52b863a73eb78d8a6f539c656089bea106bfae10
vjos <jventers@protonmail.com>
2022-06-13 00:38:40 +0100

    added no-cache argument

--- compose.sh
+++ compose.sh
@@ -1,5 +1,16 @@
 docker-compose down
-docker-compose build --no-cache
+
+if [ -z "$1" ]
+then
+	docker-compose build
+elif [ "$1" = "no-cache" ]
+then
+	docker-compose build --no-cache
+else
+	echo "Unknown argument!"
+	exit 1
+fi
+
 docker-compose up --detach
 
-alacritty --command docker-compose exec neovim /bin/sh &
+${TERM} -e docker-compose exec neovim /bin/bash &
