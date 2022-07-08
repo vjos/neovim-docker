9a6b8d989d2be1d0745bda5edb5165c432e22471
vjos <jventers@protonmail.com>
2022-07-08 17:20:06 +0100

    implemented safe_require function that prevents docker build from crashing if lua dependencies are missing

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,2 +1,22 @@
-require('color-picker').setup()
-require('zen-mode').setup()
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+function require_setup(plugin, config)
+	require(plugin).setup(config)
+end
+
+function safe_require(plugin, config)
+	config = config or {} 
+	if not pcall(require_setup, plugin, config) then
+		print('Warning: Plugin ' .. plugin ' not found.')
+	end
+end
+
+safe_require('color-picker', {})
+safe_require('zen-mode', {})
+
